69e72d6e29ea022dee9d9debeb6f7235b3d39bc2
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-10-16 13:35:27 +0200

    Estructuracion de carpetas

---
+++ src/components/usuarios/Alta_usuarios.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+
+class Alta_usuarios extends React.Component{
+    
+    render(){
+        return(
+            <main>
+                <p>Hola para ti mi cola</p>
+            </main>
+        )
+    }
+}
+export default Alta_usuarios;
