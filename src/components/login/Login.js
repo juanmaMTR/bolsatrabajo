7ae7fc0fa24c5f7cde61b935ba83c30b79918bdc
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-10-19 00:35:38 +0200

    manda datos por post en formato json

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, {useRef} from "react";
 import ErrorForms from "../componentesBasicos/ErrorForms";
 
 
-class Login extends React.Component{
-    render(){
-        return(
-            <main>
-                <h1>Login</h1>
-                <form action="#" method="POST" onSubmit={this.handleSubmit}>
-                    <label>Usuario: </label>
-                    <input type="text" name="nombre" onChange={this.handleChange}/><br/>
-                    <label>Contraseña: </label>
-                    <input type="text" name="correo" onChange={this.handleChange}/><br/>
-                    <input type="submit" value="Acceder" name="Login"/>
-                </form>
-            </main>
-        )
+const Login  = () => {   
+    
+
+    const inputUsuario = useRef(null)
+    const inputContrasenia = useRef(null)
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+        
+        const datosPeticion = {
+            titulo: 'Datos formulario login', 
+            usuario: inputUsuario.current.value, 
+            contrasenia: inputContrasenia.current.value
+        }
+        const opcionesPeticion = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(datosPeticion)
+        }
+        fetch('/php/controller/controller.php', opcionesPeticion)
+           .then(response => response.json())
+
+        console.log(inputUsuario.current.value);
+        console.log(inputContrasenia.current.value);
     }
+    return(
+        <main>
+            <h1>Login</h1>
+            <form action="#" method="POST" onSubmit={handleSubmit}>
+                <label>Usuario</label><br/>
+                <input type="text" name="nombre" placeholder="Introduzca el usuario" ref={inputUsuario}/><br/>
+                <label>Contraseña</label><br/>
+                <input type="password" name="contrasenia" placeholder="Introduzca la contraseña" ref={inputContrasenia}/><br/>
+                <input type="submit" value="Acceder" name="Login"/>
+            </form>
+        </main>
+    )
+        
+    
 }
 export default Login 
