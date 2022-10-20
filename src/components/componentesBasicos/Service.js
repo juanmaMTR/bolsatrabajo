f55617be177eed8a97bdfefa805aab12f299ae95
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-10-20 17:45:02 +0200

    merge

--- src/components/componentesBasicos/Service.js
+++ src/components/componentesBasicos/Service.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Service = (parametros) => {
 
     console.log(parametros)
@@ -17,38 +16,3 @@ const Service = (parametros) => {
 
 }
 export default Service
-=======
-import React from 'react';
-import { environment } from './environment';
-
-const url = environment.apiURl + 'index.php';
-export default class Service extends React.Component {
-    constructor(props) {
-        super(props);
-        const datos = props => props.datos;
-        fetch( url, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify( datos )
-        }).then(
-            (response) => {return response}
-        )
-    }
-    /*servicio(  ){
-        const datos = props => props.datos;
-        fetch( url, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify( datos )
-        }).then(
-            (response) => {return response}
-        )
-    }*/
-}
->>>>>>> 496bf46250bdc649c18d56a5afbb05021fa3f865
