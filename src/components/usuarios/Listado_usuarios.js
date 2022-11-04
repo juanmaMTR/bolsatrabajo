b449a63cbeb024f3229d25c5cb47b22c2730b1df
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-11-04 20:13:01 +0100

    falta perfilar listado, css hecho

--- src/components/usuarios/Listado_usuarios.js
+++ src/components/usuarios/Listado_usuarios.js
@@ -5,9 +5,6 @@ import '../../css/listado.css'
 
 const ListadoUsaurios = () =>{
     
-    // async actualizarLista(datosResponse){
-    //     this.setState({lista: datosResponse})
-    // }
     const [lista, setLista] = useState([]);
     const [usuario,setUsuario] = useState(1);
 
@@ -26,20 +23,44 @@ const ListadoUsaurios = () =>{
 
         datosResponse.forEach(arrayDatos => {
             resultado.push(
-                <div>
-                    <p>Usuario: {arrayDatos.nombreUsuario} -- Estado: {arrayDatos.estado} -- DNI: {arrayDatos.dni} -- Correo: {arrayDatos.correo} -- Nombre: {arrayDatos.nombre} -- Apellidos: {arrayDatos.apellidos} -- Primera vez: {arrayDatos.primeraVez}</p>
-                </div>
+                <tr class="bg-gray-800">
+                    <td class="p-3">
+                        <div class="flex align-items-center">
+                            <img class="rounded-full h-12 w-12  object-cover" src="https://www.safa.edu/images/galerias/home/4-fundacion-Loyola.jpg" alt="unsplash image"/>
+                            <div class="ml-3">
+                                <div class="">{arrayDatos.nombreUsuario}</div>
+                                <div class="text-gray-500">{arrayDatos.correo}</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="p-3">
+                        <div class="">{arrayDatos.nombre}</div>
+                        <div class="text-gray-500">{arrayDatos.apellidos}</div>
+                    </td>
+                    <td class="p-3 font-bold">
+                        {arrayDatos.dni}
+                    </td>
+                    <td class="p-3">
+                        <span class="bg-green-400 text-gray-50 rounded-md px-2">{arrayDatos.estado}</span>
+                    </td>
+                    <td class="p-3 ">
+                        <a href="#" class="text-gray-400 hover:text-gray-100 mr-2">
+                            <i class="material-icons-outlined text-base">visibility</i>
+                        </a>
+                        <a href="#" class="text-gray-400 hover:text-gray-100  mx-2">
+                            <i class="material-icons-outlined text-base">edit</i>
+                        </a>
+                        <a href="#" class="text-gray-400 hover:text-gray-100  ml-2">
+                            <i class="material-icons-round text-base">delete_outline</i>
+                        </a>
+                    </td>
+                </tr>
             )
         });
         console.log(resultado);
         setLista(resultado)
 
     }
-    // const componenteLista= [
-    //     for(let i=0; i<lista.length; i++){
-    //         <p>lista[i]['nombreUsuario']</p>,
-    //     }
-    // ];
     // const BorrarUsuario = async (nombreUsuario)=>{
     //     const parametros = {
     //         method: 'POST',
@@ -52,63 +73,28 @@ const ListadoUsaurios = () =>{
     //     const datosResponse = await response.json()
     //     console.log(datosResponse);
     // }
-    // const handleUsuario= ()=> {
-    //     for (let index = 0; index < lista.length; index++) {
-    //         setUsuario(lista[index])
-    //     }
-    // }
+    
     return(
         <div>
             <h2>Listado</h2>
             <button onClick={ListarUsuarios}>
                 Listar
             </button>
-            {lista}
             <div class="flex items-center justify-center min-h-screen bg-gray-900">
                 <div class="col-span-12">
                     <div class="overflow-auto lg:overflow-visible ">
                         <table class="table text-gray-400 border-separate space-y-6 text-sm">
                             <thead class="bg-gray-800 text-gray-500">
                                 <tr>
-                                    <th class="p-3">Brand</th>
-                                    <th class="p-3 text-left">Category</th>
-                                    <th class="p-3 text-left">Price</th>
-                                    <th class="p-3 text-left">Status</th>
+                                    <th class="p-3">Usuario</th>
+                                    <th class="p-3 text-left">Nombre/Apellidos</th>
+                                    <th class="p-3 text-left">DNI</th>
+                                    <th class="p-3 text-left">Estado</th>
                                     <th class="p-3 text-left">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr class="bg-gray-800">
-                                    <td class="p-3">
-                                        <div class="flex align-items-center">
-                                            <img class="rounded-full h-12 w-12  object-cover" src="https://images.unsplash.com/photo-1613588718956-c2e80305bf61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="unsplash image"/>
-                                            <div class="ml-3">
-                                                <div class="">Appple</div>
-                                                <div class="text-gray-500">mail@rgmail.com</div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="p-3">
-                                        Technology
-                                    </td>
-                                    <td class="p-3 font-bold">
-                                        200.00$
-                                    </td>
-                                    <td class="p-3">
-                                        <span class="bg-green-400 text-gray-50 rounded-md px-2">available</span>
-                                    </td>
-                                    <td class="p-3 ">
-                                        <a href="#" class="text-gray-400 hover:text-gray-100 mr-2">
-                                            <i class="material-icons-outlined text-base">visibility</i>
-                                        </a>
-                                        <a href="#" class="text-gray-400 hover:text-gray-100  mx-2">
-                                            <i class="material-icons-outlined text-base">edit</i>
-                                        </a>
-                                        <a href="#" class="text-gray-400 hover:text-gray-100  ml-2">
-                                            <i class="material-icons-round text-base">delete_outline</i>
-                                        </a>
-                                    </td>
-                                </tr>
+                                {lista}
                                 <tr class="bg-gray-800">
                                     <td class="p-3">
                                         <div class="flex align-items-center">
