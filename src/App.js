969d54ac1f0809433d21652fbcf47a4e12531a40
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-11-06 00:29:10 +0100

    Listar usuarios y borrado hecho con pop up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Alta_usuarios from './components/usuarios/Alta_usuarios';
 import Login from './components/login/Login';
-import ListadoUsaurios from './components/usuarios/Listado_usuarios';
+import ListadoUsuarios from './components/usuarios/Listado_usuarios';
 
 function App() {
   return (
     <div className="App">
       {/* <Alta_usuarios/> */}
-      <ListadoUsaurios/>
+      <ListadoUsuarios/>
     </div>
   );
 }
--- src/components/usuarios/Listado_usuarios.js
+++ src/components/usuarios/Listado_usuarios.js
@@ -1,17 +1,25 @@
-import {useState, useEffect} from "react";
+import React,{useState, useEffect} from "react";
 import Service from "../componentesBasicos/Service";
 import '../../css/listado.css'
 
 
-const ListadoUsaurios = () =>{
+const ListadoUsuarios = () =>{
     
     const [lista, setLista] = useState([]);
-    const [usuario,setUsuario] = useState(1);
 
-   
+    const [mostrar, setMostrar] = useState(false);
+    const [popUp, setPopUp] = useState(false)
+
+    
+
+    let usuario
+    useEffect(()=>{
+        ListarUsuarios()
+    }, [])
     const ListarUsuarios = async ()=>{
         const parametros = {
             method: 'POST',
+            url: '../src/php/index.php',
             inputs: {
                 accion: 'listado_usuarios'
             }
@@ -20,8 +28,45 @@ const ListadoUsaurios = () =>{
         const datosResponse = await response.json();
         const resultado = []
         console.log(datosResponse);
+        
+        let estadoTrabajo
+        datosResponse.forEach(arrayDatos => {   
+            const borrarUsuario = () =>{
+                const peticionBorrar = () =>{
+                    const parametrosPeticion = {
+                        method: 'POST',
+                        url: '../src/php/index.php',
+                        inputs: {
+                            accion: 'borrar_usuario',
+                            nombreUsuario: arrayDatos.nombreUsuario
+                        }
+                    }
+                    Service(parametrosPeticion)
+                    setTimeout(() => {
+                        ListarUsuarios()
+                    }, 200);                    
+                }
 
-        datosResponse.forEach(arrayDatos => {
+                const Modal = 
+                    <div class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
+                        <div class="bg-white px-16 py-14 rounded-md text-center">
+                            <h1 class="text-xl mb-4 font-bold text-slate-500">¿Desea borrar el usuario {arrayDatos.nombreUsuario}?</h1>
+                            <button onClick={()=>{setPopUp(false)}} class="bg-red-500 px-4 py-2 rounded-md text-md text-white">No</button>
+                            <button onClick={()=>{setPopUp(false); peticionBorrar()}} class="bg-green-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold">Sí</button>
+                        </div>
+                    </div>
+            
+                setPopUp(Modal)
+                usuario = arrayDatos.nombreUsuario
+                console.log(usuario)
+            }
+            if (arrayDatos.estado == 1) {
+                arrayDatos.estado = "Trabajando"                        
+                estadoTrabajo = <span class="bg-green-400 text-gray-50 rounded-md px-2">{arrayDatos.estado}</span>
+            }else{
+                arrayDatos.estado = "No trabajando"
+                estadoTrabajo = <span class="bg-red-400 text-gray-50 rounded-md px-2">{arrayDatos.estado}</span>
+            }            
             resultado.push(
                 <tr class="bg-gray-800">
                     <td class="p-3">
@@ -41,130 +86,52 @@ const ListadoUsaurios = () =>{
                         {arrayDatos.dni}
                     </td>
                     <td class="p-3">
-                        <span class="bg-green-400 text-gray-50 rounded-md px-2">{arrayDatos.estado}</span>
+                        {estadoTrabajo}
                     </td>
                     <td class="p-3 ">
-                        <a href="#" class="text-gray-400 hover:text-gray-100 mr-2">
+                        <button href="#" class="text-gray-400 hover:text-gray-100 mr-2">
                             <i class="material-icons-outlined text-base">visibility</i>
-                        </a>
-                        <a href="#" class="text-gray-400 hover:text-gray-100  mx-2">
+                        </button>
+                        <button href="#" class="text-gray-400 hover:text-gray-100  mx-2">
                             <i class="material-icons-outlined text-base">edit</i>
-                        </a>
-                        <a href="#" class="text-gray-400 hover:text-gray-100  ml-2">
+                        </button>
+                        <button onClick={()=>{setMostrar(true); borrarUsuario()}} class="text-gray-400 hover:text-gray-100  ml-2">
                             <i class="material-icons-round text-base">delete_outline</i>
-                        </a>
+                        </button>
                     </td>
                 </tr>
             )
         });
         console.log(resultado);
         setLista(resultado)
-
-    }
-    // const BorrarUsuario = async (nombreUsuario)=>{
-    //     const parametros = {
-    //         method: 'POST',
-    //         inputs:{
-    //             accion: 'borrar_usuario',
-    //             nombreUsuario: nombreUsuario
-    //         }
-    //     }
-    //     const response = await Service(parametros)
-    //     const datosResponse = await response.json()
-    //     console.log(datosResponse);
-    // }
+    }    
+   
     
     return(
-        <div>
-            <h2>Listado</h2>
-            <button onClick={ListarUsuarios}>
-                Listar
-            </button>
-            <div class="flex items-center justify-center min-h-screen bg-gray-900">
-                <div class="col-span-12">
-                    <div class="overflow-auto lg:overflow-visible ">
-                        <table class="table text-gray-400 border-separate space-y-6 text-sm">
-                            <thead class="bg-gray-800 text-gray-500">
-                                <tr>
-                                    <th class="p-3">Usuario</th>
-                                    <th class="p-3 text-left">Nombre/Apellidos</th>
-                                    <th class="p-3 text-left">DNI</th>
-                                    <th class="p-3 text-left">Estado</th>
-                                    <th class="p-3 text-left">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {lista}
-                                <tr class="bg-gray-800">
-                                    <td class="p-3">
-                                        <div class="flex align-items-center">
-                                            <img class="rounded-full h-12 w-12   object-cover" src="https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="unsplash image"/>
-                                            <div class="ml-3">
-                                                <div class="">Realme</div>
-                                                <div class="text-gray-500">mail@rgmail.com</div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="p-3">
-                                        Technology
-                                    </td>
-                                    <td class="p-3 font-bold">
-                                        200.00$
-                                    </td>
-                                    <td class="p-3">
-                                        <span class="bg-red-400 text-gray-50 rounded-md px-2">no stock</span>
-                                    </td>
-                                    <td class="p-3">
-                                        <a href="#" class="text-gray-400 hover:text-gray-100  mr-2">
-                                            <i class="material-icons-outlined text-base">visibility</i>
-                                        </a>
-                                        <a href="#" class="text-gray-400 hover:text-gray-100 mx-2">
-                                            <i class="material-icons-outlined text-base">edit</i>
-                                        </a>
-                                        <a href="#" class="text-gray-400 hover:text-gray-100 ml-2">
-                                            <i class="material-icons-round text-base">delete_outline</i>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr class="bg-gray-800">
-                                    <td class="p-3">
-                                        <div class="flex align-items-center">
-                                            <img class="rounded-full h-12 w-12   object-cover" src="https://images.unsplash.com/photo-1600856209923-34372e319a5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2135&q=80" alt="unsplash image"/>
-                                            <div class="ml-3">
-                                                <div class="">Samsung</div>
-                                                <div class="text-gray-500">mail@rgmail.com</div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="p-3">
-                                        Technology
-                                    </td>
-                                    <td class="p-3 font-bold">
-                                        200.00$
-                                    </td>
-                                    <td class="p-3">
-                                        <span class="bg-yellow-400 text-gray-50  rounded-md px-2">start sale</span>
-                                    </td>
-                                    <td class="p-3">
-                                        <a href="#" class="text-gray-400 hover:text-gray-100 mr-2">
-                                            <i class="material-icons-outlined text-base">visibility</i>
-                                        </a>
-                                        <a href="#" class="text-gray-400 hover:text-gray-100 mx-2">
-                                            <i class="material-icons-outlined text-base">edit</i>
-                                        </a>
-                                        <a href="#" class="text-gray-400 hover:text-gray-100 ml-2">
-                                            <i class="material-icons-round text-base">delete_outline</i>
-                                        </a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+        <div class="flex items-center justify-center min-h-screen bg-gray-900">
+            <div class="col-span-12">
+                <div class="overflow-auto lg:overflow-visible ">
+                    <table class="table text-gray-400 border-separate space-y-6 text-sm">
+                        <thead class="bg-gray-800 text-gray-500">
+                            <tr>
+                                <th class="p-3">Usuario</th>
+                                <th class="p-3">Nombre/Apellidos</th>
+                                <th class="p-3">DNI</th>
+                                <th class="p-3">Estado</th>
+                                <th class="p-3">Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {lista}
+                        </tbody>
+                    </table>
                 </div>
             </div>
+
+            {mostrar && popUp}
         </div>
     )
     
 }
 
-export default ListadoUsaurios
+export default ListadoUsuarios
