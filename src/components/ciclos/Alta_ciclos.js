25dcf64748bb4b05d72669c556f81c29f748cd77
Juan Manuel Toscano Reyes <91120972+juanmaMTR@users.noreply.github.com>
2022-11-15 09:31:55 +0000

    Alta ciclo

---
+++ src/components/ciclos/Alta_ciclos.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const Alta_ciclos = () => {
+    return(
+        <div class="bg-gray-200 bg-opacity-50 flex justify-center items-center">
+            <div class="w-full max-w-xs">
+                <h1 class="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">Alta de Ciclos</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Alta_ciclos;
