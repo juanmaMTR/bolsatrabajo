56636fa90fef713c4f3e0f8b3fb7e656ca4beb2b
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-11-20 18:32:05 +0100

    config

--- src/php/config.php
+++ src/php/config.php
@@ -1,11 +1,11 @@
 <?php
-    define('HOSTNAME','127.0.0.1');
-    define('USERNAME','user2daw_21');
-    define('PASSWORD','m$0_kO5xRb1u');
-    define('DATABASE','user2daw_BD2-21');
-
     // define('HOSTNAME','127.0.0.1');
-    // define('USERNAME','root');
-    // define('PASSWORD','');
-    // define('DATABASE','BolsaTrabajo');
+    // define('USERNAME','user2daw_21');
+    // define('PASSWORD','m$0_kO5xRb1u');
+    // define('DATABASE','user2daw_BD2-21');
+
+    define('SERVER','127.0.0.1');
+    define('USER','root');
+    define('PW','');
+    define('DB','BolsaTrabajo');
 ?>
